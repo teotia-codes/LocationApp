9975ff409b7b05e10ab58eed77111865f4539819
teotia-codes <priyanshu.teotia50@gmail.com>
2024-03-01 20:01:08 +0530

    Checkpoint

--- app/src/main/java/com/example/locationapp/LocationUtils.kt
+++ app/src/main/java/com/example/locationapp/LocationUtils.kt
@@ -1,10 +1,40 @@
 package com.example.locationapp
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Looper
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 
 class LocationUtils(val context: Context) {
+    private val _fusedLocationClient: FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(context)
+   @SuppressLint("MissingPermission")
+    fun requestLocationUpdates(viewModel: LocationViewModel){
+          val locationCall = object : LocationCallback(){
+              override fun onLocationResult(locationResult: LocationResult) {
+                  super.onLocationResult(locationResult)
+                  locationResult.lastLocation?.let {
+                      val location = LocationData(
+                          latitude = it.latitude,
+                          longitude = it.longitude)
+                      viewModel.updateLocation(location)
+                  }
+              }
+          }
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY,1000
+        ).build()
+        _fusedLocationClient.requestLocationUpdates(locationRequest,
+            locationCall,
+            Looper.getMainLooper())
+    }
     fun hasLocationPermission(context: Context): Boolean {
         return ContextCompat.checkSelfPermission(
                 context,
--- app/src/main/java/com/example/locationapp/MainActivity.kt
+++ app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.locationapp
 
 import android.content.Context
 import android.Manifest
+
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -17,20 +19,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.locationapp.ui.theme.LocationAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel : LocationViewModel = viewModel()
             LocationAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-            
+                    MyApp(viewModel)
                 }
             }
         }
@@ -38,18 +44,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MyApp(viewModel: LocationViewModel){
+    val context = LocalContext.current
+    val locationUtils = LocationUtils(context)
+
+    LocationDisplay(locationUtils = locationUtils, context = context,
+        viewModel  )
+}
+
+@Composable
+//Similiar for all the permissions
 fun LocationDisplay(
     locationUtils: LocationUtils,
-    context: Context){
+    context: Context,
+    viewModel: LocationViewModel){
+    val location = viewModel.location.value
     val requestPermissionLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestMultiplePermissions(),
         onResult = {
             permissions ->
             if(permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
                 && permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true){
-
+              locationUtils.requestLocationUpdates(viewModel=viewModel)
             }else{
-
+                  val rationaleReq = ActivityCompat.shouldShowRequestPermissionRationale(
+                      context as MainActivity,
+                      Manifest.permission.ACCESS_FINE_LOCATION
+                  ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                      context,
+                      Manifest.permission.ACCESS_COARSE_LOCATION
+                  )
+                if(rationaleReq){
+                    Toast.makeText(context,
+                        "Location Permission is req for this feature to work",
+                        Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(context,
+                        "Go to the settings to enable location services",
+                        Toast.LENGTH_LONG).show()
+                }
             }
         })
     
@@ -58,16 +91,26 @@ fun LocationDisplay(
     Column(modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center) {
-Text(text = "Location not available",
-    fontWeight = FontWeight.Bold,
-    )
+        if(location != null){
+            Text(text = "Address: ${location.latitude} ${location.longitude}")
+        }else{
+            Text(text = "Location not available",
+                fontWeight = FontWeight.Bold,
+            )
+        }
+
         Button(onClick ={
             if(locationUtils.hasLocationPermission(context)){
-                // Permission already granted
-                
+                // Permission already granted update the location
+                locationUtils.requestLocationUpdates(viewModel)
             }else{
                 //Request location permission
-                
+                requestPermissionLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                    )
+                )
             }
         }) {
 Text(text = "Get Location")
